d3712b7c7f1dd574a7d84543e06fdd7f128fbc27
Akeanant Poomdeesittinon <aekanan2004@gmail.com>
2026-02-22 20:08:00 +0700

    feat: enable CORS

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/tony219y/pomo-smart-task-api/config/db"
 	"github.com/tony219y/pomo-smart-task-api/internal/handler"
 	"github.com/tony219y/pomo-smart-task-api/internal/middleware"
@@ -16,13 +17,20 @@ func main() {
 	database := db.ConnectNeon()
 	db.Migration(database)
 
+	app.Use(cors.New())
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+	}))
+
 	userRepo := repository.NewUserRepository(database)
 	userService := service.NewUserService(userRepo)
 	userHandler := handler.NewUserHandler(userService)
 
 	auth := app.Group("/api/v1/auth")
 	auth.Post("/register", userHandler.CreateUser)
-	auth.Get("/login", userHandler.UserLogin)
+	auth.Post("/login", userHandler.UserLogin)
 
 	api := app.Group("/api/v1", middleware.JWTMiddleware)
 	//Users
